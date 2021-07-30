4bdc8d5daa387725b8caf3244ed71fd685dfde66
rogerdurdn <roo@ROGERS.localdomain>
2021-07-29 21:19:26 -0500

    some content to test

---
+++ some.php
@@ -0,0 +1,18 @@
+this is a example inside a file
+
+
+function  func1(my funcy){
+now something
+}
+
+function  func2(my funcy){
+now something
+}
+
+function noFuncion(my funcy){
+now something
+}
+
+Function noFuncion(my funcy){
+now something
+}
